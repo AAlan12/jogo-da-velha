6e391b791fa4d7d0738d6338e2097cc88f2157c2
Antonio Alan <tonio.alan07@gmail.com>
2023-03-25 11:18:40 -0300

    ajuste no obj menu

--- src/objects/Menu/index.js
+++ src/objects/Menu/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import './styles.css';
 
 const Menu = () => {
-    <a href="#menu" className="menu"><span className="center">Menu</span></a>
+    return(
+        <a href="#menu" className="menu"><span className="center">Menu</span></a>
+    )
 }
 
 export default Menu;
