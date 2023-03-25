ba3dfbee83d5ba5e2472757b55e909d40231bf1f
Antonio Alan <tonio.alan07@gmail.com>
2023-03-24 22:23:27 -0300

    criação do obj Logo

---
+++ src/objects/Logo/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles.css';
+import AppLogo from "../../img/react.png";
+
+const Logo = () => {
+    return(
+        <img src={AppLogo} className="logo" alt="" />
+    )
+}
+
+export default Logo;
