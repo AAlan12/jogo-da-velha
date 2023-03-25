c611b763ad2e99f9c7c68567f8586bd04061cfd3
Antonio Alan <tonio.alan07@gmail.com>
2023-03-24 21:55:36 -0300

    otimização de pastas e criação do obj Sobre

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './styles/elements/base.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
