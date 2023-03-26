0844a795a66601d0dcd6c8bb9cca5e05e512f87d
Antonio Alan <tonio.alan07@gmail.com>
2023-03-26 08:07:57 -0300

    ajustes no header com styles css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "../../objects/Logo";
 import Menu from "../../objects/Menu";
 import Sobre from "../../objects/Sobre";
+import './styles.css';
  const Header = () => {
     return(
         <header className="header">
