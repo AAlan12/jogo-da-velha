89465b6c26bb21aa350f2e6008a75989b42a9bc7
Antonio Alan <tonio.alan07@gmail.com>
2023-03-26 23:53:55 -0300

    criando component Hashtag

---
+++ src/components/Hashtag/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.css';
+import Card from '../../objects/Card';
+import Player from '../../objects/Player';
+
+
+const Hashtag = () => {
+    return(
+        <Card>
+            <Player player="o"/>
+        </Card>
+    )
+}
+export default Hashtag;
