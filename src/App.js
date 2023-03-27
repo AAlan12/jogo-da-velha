9d4b338cbd4eeeeab1c63f4253ee4c2e4c5ec5bc
Antonio Alan <tonio.alan07@gmail.com>
2023-03-27 13:45:14 -0300

    Criação do obj Label

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header';
+import Label from './objects/Label';
 function App() {
   return (
     <main className='app'>
       <Header/>
       <Hashtag/>  
+      <Label content="Mostrar eventos"/>
     </main>
   );
 }
---
+++ src/objects/Label/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Label = ({ content }) => {
+    return(
+        <label>
+            {content}
+        </label>
+    )
+}
+
+export default Label
