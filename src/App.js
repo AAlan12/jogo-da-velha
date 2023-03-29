85fde1bc9f40b446719df91b236ab333ecec0a5f
Antonio Alan <tonio.alan07@gmail.com>
2023-03-29 06:38:02 -0300

    Criação do obj input

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header';
+import Input from './objects/Input';
 import Label from './objects/Label';
 function App() {
   return (
     <main className='app'>
       <Header/>
       <Hashtag/>  
+      <Input value="show"/>
       <Label content="Mostrar eventos"/>
     </main>
   );
