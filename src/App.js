14d4154e2957cf25b34eb25432357a8f31524175
Antonio Alan <tonio.alan07@gmail.com>
2023-03-30 00:48:49 -0300

    Associar label com input

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header';
 import Input from './objects/Input';
-import Label from './objects/Label';
 function App() {
   return (
     <main className='app'>
       <Header/>
       <Hashtag/>  
-      <Input value="show"/>
-      <Label content="Mostrar eventos"/>
+      <Input id="show" value="show" content="Mostrar eventos"/>
     </main>
   );
 }
